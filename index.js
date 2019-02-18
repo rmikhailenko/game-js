db580dc4abc942573d9870d14d5dd1d38c465708
Roman Mikhailenko <mikhoilenko.roman@gmail.com>
2019-02-18 19:29:27 +0200

    update class Square

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 class Square {
   constructor (maxX) {
-    this.x = Random.getNumber(0, this.maxX)
+    this.x = Random.getNumber(0, maxX)
     this.y = 0
-    this.side = 20
+    this.side = Random.getNumber(7, 20)
     this.color = Random.getColor()
     this.speed = Random.getNumber(1, 6)
   }
